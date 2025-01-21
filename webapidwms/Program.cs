7eaacb9a36690370f5022cff26db4c38c0f62261
karthikcng <113571412+karthikcng@users.noreply.github.com>
2025-01-21 18:48:59 +0530

    Update Program.cs

--- webapidwms/Program.cs
+++ webapidwms/Program.cs
@@ -17,11 +17,11 @@ namespace webapidwms
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+            // if (app.Environment.IsDevelopment())
+            // {
+            //     app.UseSwagger();
+            //     app.UseSwaggerUI();
+            // }
             app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseSwagger();
             app.UseSwaggerUI();
